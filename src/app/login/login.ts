85040c0ff61dea4aae9feb7a24c87fe0c2052703
LAPTOP-A83LG6TQ\User <tauqeerulhassan45@gmail.com>
2025-11-13 21:10:49 +0000

    setting and getting text

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -8,8 +8,24 @@ import { Component } from '@angular/core';
 })
 export class Login {
   name:string | number = "20"
+  userName = ""
   clickMe(){
     console.log("Button is Clicked")
     this.name = 100
   }
+
+  setText(){
+    this.userName = "Tauqeer ul Hassan"
+  }
+
+  getUserName(event:Event){
+    const name = (event.target as HTMLInputElement).value
+    this.userName = name
+  }
+
+  getUserNameWithTemplate(value:string){
+    console.log(`Name entered is: ${value}`);
+
+  }
+
 }
