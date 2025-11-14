a160dc7634f1e45e67cd234f3085f55c6b913d2c
LAPTOP-A83LG6TQ\User <tauqeerulhassan45@gmail.com>
2025-11-14 14:52:15 +0000

    new components added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Login } from './login/login';
 import { Counter } from './counter/counter';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Login,Counter],
+  imports: [Login,Counter,RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
